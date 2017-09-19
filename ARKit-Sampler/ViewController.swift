3af870f60e17c1c2733d7dd8f4fe84523875172d
shu223 <shuichi0526@gmail.com>
2017-09-20 02:40:23 +0900

    Initial Commit

---
+++ ARKit-Sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKit-Sampler
+//
+//  Created by Shuichi Tsutsumi on 2017/09/20.
+//  Copyright © 2017 Shuichi Tsutsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
